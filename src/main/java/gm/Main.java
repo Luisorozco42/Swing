b96bb38f259ec182d894ea406eb3ae91714de1bd
Luisorozco42 <lorozcolazo@gmail.com>
2026-05-14 18:01:08 -0600

    using dark mode with swing

--- src/main/java/gm/Main.java
+++ src/main/java/gm/Main.java
@@ -1,11 +1,13 @@
 package gm;
 
+import com.formdev.flatlaf.FlatDarculaLaf;
 import ui.Forma;
 
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     static void main() {
+        FlatDarculaLaf.setup();// Laf signigica look and feel (tecnicamente es para poner en modo oscuro)
         Forma forma = new Forma();
 
         forma.setVisible(true);
--- src/main/java/ui/Forma.java
+++ src/main/java/ui/Forma.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 public class Forma extends JFrame {
 
-    private JPanel panel1; //si el nombre de la variable es el mismo que el field name se asocia directamente
+    private JPanel panelPrincipal; //si el nombre de la variable es el mismo que el field name se asocia directamente
 
 
     public Forma(){
@@ -12,7 +12,7 @@ public class Forma extends JFrame {
     }
 
     private void inicializarForma() {
-        setContentPane(panel1);
+        setContentPane(panelPrincipal);
         setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         setSize(600,400);
         setLocationRelativeTo(null);//Centramos la ventana
