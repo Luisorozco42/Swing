cd47889d3b3ad317f0e4637cd6a0112947d3363e
Luisorozco42 <lorozcolazo@gmail.com>
2026-05-14 18:13:50 -0600

    learning about action listeners

--- src/main/java/ui/Forma.java
+++ src/main/java/ui/Forma.java
@@ -1,14 +1,21 @@
 package ui;
 
 import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 
 public class Forma extends JFrame {
 
     private JPanel panelPrincipal; //si el nombre de la variable es el mismo que el field name se asocia directamente
+    private JTextField campoTexto;
+    private JLabel replicadorLable;
 
 
     public Forma(){
         inicializarForma();
+        campoTexto.addActionListener(e -> { // tecnicamente se esta llamando cuando doy enter
+            System.out.println("Se ejecuto el action listener");
+        });
     }
 
     private void inicializarForma() {
